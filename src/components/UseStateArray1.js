635b3b2c555ea72a0f3f40d6633d26545404e66c
Tareq1131 <tareqislam076@gmail.com>
2024-01-10 23:48:07 +0600

    useState array

---
+++ src/components/UseStateArray1.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const useStateArray = () => {
+  const [items, setItems] = useState([]);
+  const additems = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.floor(Math.random() * 10) + 1,
+      },
+    ]);
+  };
+  return (
+    <div>
+      <button onClick={additems}>Add a button</button>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default useStateArray;
